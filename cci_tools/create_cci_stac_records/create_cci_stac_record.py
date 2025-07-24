0398c0eb4f95f6f6c666cb88c2ebbe28bd40c76b
knappett <diane.knappett@stfc.ac.uk>
2025-07-24 18:17:33 +0100

    Added functionality to create STAC records for GeoTIFF files.

--- cci_tools/create_cci_stac_records/create_cci_stac_record.py
+++ cci_tools/create_cci_stac_records/create_cci_stac_record.py
@@ -6,15 +6,16 @@ and then converts the opensearch record output to STAC format.
 Currently run from the command line using:
 python create_cci_stac_record.py cci_dir
 
-e.g. python create_cci_stac_record.py /neodc/esacci/biomass/data/agb/maps/v6.0/netcdf
+e.g. python create_cci_stac_record.py /neodc/esacci/biomass/data/agb/maps/v6.0/netcdf /gws/nopw/j04/esacci_portal/stac/stac_records
 """
 from elasticsearch import Elasticsearch
 import elasticsearch.helpers
-import pandas as pd
 import json 
 import requests
 import os
 import click
+import pdb
+import rasterio
 
 def get_query(directory):
     query = {
@@ -37,23 +38,43 @@ def get_query(directory):
     }
     return query
 
-def process_record(es_all_dict:dict, output_dir:str):
-    #=== id ===
+def extract_id(es_all_dict:dict):
+    """
+    Extract filename from OpenSearch record
+    """
     fname = str(es_all_dict['info'].get('name'))
     file_id, file_ext = os.path.splitext(fname)
-    #id = fname.partition('.nc')[0]
 
-    #=== Collection ===
+    return fname, file_id, file_ext
+
+def extract_collection(es_all_dict:dict):
+    """
+    Extract collection from OpenSearch record
+    """
     ecv = es_all_dict['projects']['opensearch'].get('ecv')
 
     if type(ecv) is list:
         if len(ecv) == 1:
             ecv = str(ecv[0]).lower()
         else:
-            print('ECV is a list!!')
-            
-    #=== bbox ===
-    # W, N, E, S
+            print("ECV is a list!!")
+    
+    return ecv
+
+def get_licence(ecv:str):
+    '''
+    Construct URL to the relevant data license on the CEDA artefacts server
+    '''    
+    for license in ['_terms_and_conditions_v2.pdf','_terms_and_conditions.pdf','.pdf']:
+        r = requests.get(f"https://artefacts.ceda.ac.uk/licences/specific_licences/esacci_{ecv}{license}")
+        if r.status_code == 200:
+            break
+    url = f"https://artefacts.ceda.ac.uk/licences/specific_licences/esacci_{ecv}{license}"
+
+    return url
+
+def extract_opensearch(es_all_dict:dict):
+    # Extract geospatial bounding box (W, N, E, S)
     coords = es_all_dict['info']['spatial']['coordinates'].get('coordinates')
     bbox_w = coords[0][0] # west
     bbox_n = coords[0][1] # north
@@ -61,33 +82,132 @@ def process_record(es_all_dict:dict, output_dir:str):
     bbox_s = coords[1][1] # south
     bbox = [bbox_w, bbox_s, bbox_e, bbox_n]
 
-    #=== Geometry ===
     geo_type = 'Polygon'
     coordinates = [[bbox_w, bbox_s], [bbox_e, bbox_s], [bbox_e, bbox_n], [bbox_w, bbox_n], [bbox_w, bbox_s]]
 
-    #=== Properties ===
-    start_datetime = str(es_all_dict['info']['temporal'].get('start_time'))
-    end_datetime = str(es_all_dict['info']['temporal'].get('end_time'))
+    # Extract specific properties required
+    sdatetime = str(es_all_dict['info']['temporal'].get('start_time'))
+    start_datetime = sdatetime.partition('+')[0]+'Z'
+    edatetime = str(es_all_dict['info']['temporal'].get('end_time'))
+    end_datetime = edatetime.partition('+')[0]+'Z'
     version = es_all_dict['projects']['opensearch'].get('productVersion')
     platforms = es_all_dict['projects']['opensearch'].get('platform')
     drs = es_all_dict['projects']['opensearch'].get('drsId')
-    uuid = es_all_dict['projects']['opensearch'].get('datasetId')
 
+    # Extract all other properties
     properties = dict()
     for property,value in es_all_dict['projects']['opensearch'].items():
         if isinstance(value, list) and len(value) == 1:
             value = value[0]
         properties[property] = value
-
-    # License    
-    for license in ['_terms_and_conditions_v2.pdf','_terms_and_conditions.pdf','.pdf']:
-        r = requests.get(f'https://artefacts.ceda.ac.uk/licences/specific_licences/esacci_{ecv}{license}')
-        if r.status_code == 200:
-            break
-    url = f'https://artefacts.ceda.ac.uk/licences/specific_licences/esacci_{ecv}{license}'
-
+        
+    # Extract format
     format = es_all_dict['info'].get('format')
+    
+    stac_info = {
+        "start_datetime":start_datetime, 
+        "end_datetime":end_datetime, 
+        "version": version, 
+        "platforms": platforms, 
+        "drs": drs, 
+        "bbox": bbox, 
+        "geo_type": geo_type, 
+        "coordinates": coordinates, 
+        "properties":properties,
+        "format": format
+        }
+    
+    return stac_info, properties
+
+def read_geotiff(geotiff_file:str):
+    # Read in releavent info from GeoTIFF file itself
+    
+    # Values to feed into proj:transform, proj:epsg, and proj:shape
+    with rasterio.open(geotiff_file) as src:
+
+        metadata = src.tags()
+        try:
+            start_datetime=metadata['time_coverage_start']
+        except Exception as e:
+            print(f"An error occured '{e}'")
+            return None, None
+
+        end_datetime=metadata['time_coverage_end']
+        version = metadata['product_version']
+        platforms = metadata['platform']
+        drs = "TEMP"
+
+        bbox_w = metadata['geospatial_lon_min'] # west
+        bbox_n = metadata['geospatial_lat_max'] # north
+        bbox_e = metadata['geospatial_lon_max'] # east
+        bbox_s = metadata['geospatial_lat_min'] # south
+        bbox = [bbox_w, bbox_s, bbox_e, bbox_n]
+
+        geo_type = 'Polygon'
+        coordinates = [[bbox_w, bbox_s], [bbox_e, bbox_s], [bbox_e, bbox_n], [bbox_w, bbox_n], [bbox_w, bbox_s]]
+        format = 'GeoTiff'
+
+        transform = [src.transform[i] for i in range(6)]
+        epsg = src.crs.to_epsg()
+        shape = [src.height, src.width]
+        properties={"proj:transform":transform, "proj:epsg":epsg, "proj:shape":shape}
+
+        stac_info = {
+            "start_datetime":start_datetime, 
+            "end_datetime":end_datetime, 
+            "version": version, 
+            "platforms": platforms, 
+            "drs": drs, 
+            "bbox": bbox, 
+            "geo_type": geo_type, 
+            "coordinates": coordinates,
+            "format": format, 
+            "transform":transform, 
+            "epsg": epsg, 
+            "shape":shape
+            }
+    
+    return stac_info, properties
+
+def process_record(es_all_dict:dict, STAC_API:str):
+    # Extract filename, file id, and file extension
+    fname, file_id, file_ext = extract_id(es_all_dict)
+
+    # Extract file path
     location = es_all_dict['info'].get('directory')
+
+    # Extract collection (ECV/project)
+    ecv = extract_collection(es_all_dict)
+
+    # Construct url to license on the CEDA archive assets server
+    url = get_licence(ecv)
+    
+    # Extract dataset ID (UUID)
+    uuid = es_all_dict['projects']['opensearch'].get('datasetId')
+
+    if file_ext=='.nc':
+        # === NetCDF ===
+        # All information can be extracted from OpenSearch record
+
+        stac_info, properties = extract_opensearch(es_all_dict)
+
+        if isinstance(stac_info, dict) == False:
+            print("Error: OpenSearch record does not contain the required information to create a STAC record.")
+            return
+
+    elif file_ext=='.tif':
+        # === GeoTIFF ===
+        # Information will be extracted from the OpenSearch record and the GeoTIFF file itself
+
+        stac_info, properties = read_geotiff(location+"/"+fname)
+
+        if isinstance(stac_info, dict) == False:
+            print(f"Error: GeoTIFF file does not contain the required information to create a STAC record: {location}/{fname}")
+            return
+
+    else:
+        print("Error: File format not recognised!")
+        return
 
     # Create a dictionary of the required STAC output
     stac_dict = {
@@ -98,43 +218,43 @@ def process_record(es_all_dict:dict, output_dir:str):
             "https://stac-extensions.github.io/classification/v1.0.0/schema.json"
             ],
         "id": file_id,
-        "collection": drs,
+        "collection": stac_info["drs"],
         "geometry": {
-            "type": geo_type,
-            "coordinates": [coordinates]
+            "type": stac_info["geo_type"],
+            "coordinates": stac_info["coordinates"]
         },
-        "bbox": bbox,
+        "bbox": stac_info["bbox"],
         "properties": {
             "datetime": None,
-            "start_datetime": start_datetime.partition('+')[0]+'Z',
-            "end_datetime": end_datetime.partition('+')[0]+'Z',
+            "start_datetime": stac_info["start_datetime"],
+            "end_datetime": stac_info["end_datetime"],
             "license": "other",
-            "version": version,
+            "version": stac_info["version"],
             "aggregation": False,
-            "platforms": platforms,
-            "collections":[ecv, uuid, drs],
+            "platforms": stac_info["platforms"],
+            "collections":[ecv, uuid, stac_info["drs"]],
             **properties
         },
         "links": [
             {
             "rel": "self",
             "type": "application/geo+json",
-            "href": "https://api.stac.ceda.ac.uk/collections/"+drs+"/items/"+file_id
+            "href": f"{STAC_API}/collections/{stac_info['drs']}/items/{file_id}"
             },
             {
             "rel": "parent",
             "type": "application/json",
-            "href": "https://api.stac.ceda.ac.uk/collections/"+drs  
+            "href": f"{STAC_API}/collections/{stac_info['drs']}" 
             },
             {
             "rel": "collection",
             "type": "application/json",
-            "href": "https://api.stac.ceda.ac.uk/collections/"+drs  
+            "href": f"{STAC_API}/collections/{stac_info['drs']}"
             },
             {
             "rel": "root",
             "type": "application/json",
-            "href": "https://api.stac.ceda.ac.uk/"
+            "href": STAC_API
             },
             {
             "rel": "license",
@@ -143,8 +263,8 @@ def process_record(es_all_dict:dict, output_dir:str):
             }
         ],
         "assets": {
-            format: {
-                "href": location+"/"+fname,
+            stac_info["format"]: {
+                "href": f"{location}/{fname}",
                 "role": [
                     "data"
                 ]
@@ -154,39 +274,28 @@ def process_record(es_all_dict:dict, output_dir:str):
 
     # Remove platform until STAC standards have been updated to allow lists of platforms. 
     # Until then, the platform list is entered as 'platforms' instead.
-    stac_dict['properties'].pop('platform')
+    if file_ext=='.nc':
+        stac_dict['properties'].pop('platform')
 
-    # Create directory for each CCI ECV/Project
-    cci_stac_dir=output_dir+ecv+'/'
-
-    if os.path.isdir(cci_stac_dir)==False:
-        try:
-            os.mkdir(cci_stac_dir)
-            print(f"Created directory '{cci_stac_dir}' successfully")
-        except PermissionError:
-            print(f"Permission denied: Unable to make '{cci_stac_dir}'")
-        except Exception as e:
-            print(f"An error occured '{e}'")
-    
-    stac_file=cci_stac_dir+'stac_'+file_id+'-'+file_ext[1:]+'.json'
-
-    # Write 'pretty print' STAC json file
-    with open(stac_file, 'w', encoding='utf-8') as file:
-        json.dump(stac_dict, file, ensure_ascii=False, indent=2)
-    
-    print('Created STAC record: '+stac_file)
-
+    return stac_dict, file_ext
 
 # Parse command line arguments using click
 @click.command()
 @click.argument('cci_dir', type=click.Path(exists=True))
+@click.argument('output_dir', type=click.Path(exists=True))
 
-##### MAIN PROGRAM #####
+def main(cci_dir, output_dir):
+    '''
+    Reads in OpenSearch records for CCI NetCDF and geotiff data.
 
-def main(cci_dir):
+    For NetCDF files, information is extracted from the OpenSearch record only.
 
-    # Define path for output stac directory/records
-    output_dir = '/gws/nopw/j04/esacci_portal/stac/stac_records/'
+    For GeoTIFF files, only partial information is available within the OpenSearch record, so additoinal metadata is extracted from the GeoTIFF file itself.
+    '''
+
+    print(f"Input CCI directory: {cci_dir}")
+    print(f"Output STAC record directory: {output_dir}")
+    STAC_API = 'https://api.stac.ceda.ac.uk'
 
     # Setup client and query elasticsearch
     client = Elasticsearch(hosts=['https://elasticsearch.ceda.ac.uk'])
@@ -196,8 +305,30 @@ def main(cci_dir):
     
     # Loop over OpenSearch records, converting each to STAC format
     for record in response:
-        process_record(record['_source'], output_dir)
+        # Process OpenSearch record
+        stac_dict, file_ext = process_record(record['_source'], STAC_API)
 
+        # Create directory for each CCI ECV/Project
+        ecv_dir=stac_dict["properties"]["collections"][0]
+        cci_stac_dir=f"{output_dir}/{ecv_dir}/"
+
+        if os.path.isdir(cci_stac_dir)==False:
+            try:
+                os.mkdir(cci_stac_dir)
+                print(f"Created directory '{cci_stac_dir}' successfully")
+            except PermissionError:
+                print(f"Permission denied: Unable to make '{cci_stac_dir}'")
+            except Exception as e:
+                print(f"An error occured '{e}'")
+    
+        # Write 'pretty print' STAC json file
+        id=stac_dict["id"]
+        stac_file=f"{cci_stac_dir}stac_{id}-{file_ext[1:]}.json"
+
+        with open(stac_file, 'w', encoding='utf-8') as file:
+            json.dump(stac_dict, file, ensure_ascii=False, indent=2)
+    
+        print(f"Created STAC record: {stac_file}")
 
 if __name__ == "__main__":
     main()

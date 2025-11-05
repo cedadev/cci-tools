e73ded735788cb2366466d82ab011500128626b5
dwest77 <daniel.westwood@stfc.ac.uk>
2025-11-05 10:38:47 +0000

    Updated comments

--- cci_tools/cli/collection_item_count.py
+++ cci_tools/cli/collection_item_count.py
@@ -3,9 +3,13 @@ from cci_tools.core.utils import recursive_find, STAC_API
 
 @click.command()
 @click.argument('collection')
+# Also count aggregated items
 @click.option('--aggs','aggregations', required=False, is_flag=True)
+# Just check if the collection contains any items
 @click.option('--check','quick_check', required=False, is_flag=True)
+# Just show the count for collections at X depth
 @click.option('--depth','depth', required=False, type=int, default=0)
+
 def main(collection, quick_count=False, quick_check=False, aggregations=False, depth=0):
     collection_item_count(collection, quick_count=quick_count, quick_check=quick_check, aggregations=aggregations, depth=depth)
 
